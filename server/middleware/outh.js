8396a94ce3c0bb72afd3cc9e820a6eaac6f8e9e9
shira mizrahi <shiramiz404@gmail.com>
2025-06-05 18:23:46 +0300

    token

---
+++ server/middleware/outh.js
@@ -0,0 +1,23 @@
+import   jwt from 'jsonwebtoken';
+const secretKey = process.env.SECRET||'SECRETKEY';
+export function verifyToken(req, res, next) {
+const token = req.header('Authorization');
+if (!token) return res.status(401).json({ error: 'Access denied' });
+try {
+ const decoded = jwt.verify(token, secretKey);
+ req.userId = decoded.userId;
+ req.role = decoded.role;
+ console.log("decoded.role:", decoded.role);
+
+ next();
+ } catch (error) {
+ res.status(401).json({ error: 'Invalid token' });
+ }
+ };
+
+export function generateToken(userId) {
+const token = jwt.sign({ userId }, secretKey, {
+ expiresIn: '1h'
+ });
+ return token;
+ }
