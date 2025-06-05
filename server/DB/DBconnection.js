b582c4f37a223827ebb52087300129e7e5487354
shira mizrahi <shiramiz404@gmail.com>
2025-06-05 18:04:16 +0300

    commit

--- server/DB/DBconnection.js
+++ server/DB/DBconnection.js
@@ -14,7 +14,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  multipleStatements: true, // חשוב להרצת כמה פקודות SQL בקובץ
+  multipleStatements: true,
 });
 
 const DB = pool.promise();
